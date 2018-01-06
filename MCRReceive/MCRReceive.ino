87aba5ee5f7c114fafc8a7d6b9434b397dd961fc
maddiemooney <m.mooney098@gmail.com>
2018-01-06 18:09:18 -0500

    one monstrosity of a switch statement

--- MCRReceive/MCRReceive.ino
+++ MCRReceive/MCRReceive.ino
@@ -1,11 +1,19 @@
 /*
- * handles receiving/translating morse code
- */
+   handles receiving/translating morse code
+*/
+
+/*
+ *******************************************************
+    variables
+ *******************************************************
+*/
 
 int unit = 500;
 int pr = 5;
 int led = 1;
 static int dstate = 0;
+int pos = 0;
+int charpos = 0;
 
 int prlow = 400;
 int prhigh = 600;
@@ -18,40 +26,175 @@ int timer1;
 int timer2;
 
 int letter[4];
+char message[100]; //going with a 100 char limit here
+
+/*
+ *******************************************************
+    function declarations
+ *******************************************************
+*/
 
 int readpr(int prev);
+String translate(int timerval, int isletter);
 
-int readpr(int prev){
+/*
+ *******************************************************
+    subroutines
+ *******************************************************
+*/
+
+int readpr(int prev) {
   int state = analogRead(pr);
-  if(prev == LOW && state >= prhigh){
+  if (prev == LOW && state >= prhigh) {
     return HIGH;
   }
-  else if(prev == HIGH && state  < prlow){
+  else if (prev == HIGH && state  < prlow) {
     return LOW;
   }
-  else{
+  else {
     return prev;
   }
 }
 
-char translate(int timerval, int isletter){
-  if(isletter){
-    if(timerval > (unit/2) && timerval < (unit*2)){ //giving it some leeway
-      //it's a dot
+String translate(int timerval, int isletter) {
+  if (charpos > 98) {
+  }
+  else {
+    if (isletter) {
+      if (timerval > (unit / 2) && timerval < (unit * 2)) { //giving it some leeway
+        //it's a dot
+        letter[pos] = 0;
+        pos += 1;
+      }
+      else if (timerval > (unit * 2)) {
+        //it's a dash
+        letter[pos] = 1;
+        pos += 1;
+      }
     }
-    else if (timerval > (unit*2)){
-      //it's a dash
+    else {
+      if (timerval > (unit * 1.5) && timerval < (unit * 4)) {
+        for (int i = pos + 1; i < 4; i++) {
+          letter[i] = 2;
+        }
+        pos = 0;
+        message[charpos] = getmorsechar(letter);
+        charpos += 1;
+      }
+      else if (timerval > (unit * 4)) {
+        //add space to message
+        message[charpos] = ' ';
+        charpos += 1;
+        pos = 0;
+      }
+      else {
+        //don't do shit
+      }
     }
   }
-  else{
-    if(timerval > (unit*1.5) && timerval < (unit*4)){ 
-      //go to next letter
-      //clear cache for letters
-    }
-    else if(timerval > (unit*4)){
-      //go to next word
-    }
-    //else it's just a space between a dash/dot, ignore
+}
+
+char getmorsechar(int letter[]) {
+  switch (letter[0]) {
+    case 0:
+      switch (letter[1]) {
+        case 0:
+          switch (letter[2]) {
+            case 0:
+              switch (letter[3]) {
+                case 0:
+                  return 'H';
+                case 1:
+                  return 'V';
+                default:
+                  return 'S';
+              }
+            case 1:
+              switch (letter[3]) {
+                case 0:
+                  return '-';
+                case 1:
+                  return 'F';
+                default:
+                  return 'U';
+              }
+            default:
+              return 'I';
+          }
+        case 1:
+          switch (letter[2]) {
+            case 0:
+              switch (letter[3]) {
+                case 0:
+                  return '?';
+                case 1:
+                  return 'L';
+                default:
+                  return 'R';
+              }
+            case 1:
+              switch (letter[3]) {
+                case 0:
+                  return 'J';
+                case 1:
+                  return 'P';
+                default:
+                  return 'W';
+              }
+            //wjp
+            default:
+              return 'A';
+          }
+        default:
+          return 'E';
+      }
+    case 1:
+      switch (letter[1]) {
+        case 0:
+          switch (letter[2]) {
+            case 0:
+              switch (letter[3]) {
+                case 0:
+                  return 'B';
+                case 1:
+                  return 'X';
+                default:
+                  return 'D';
+              }
+            case 1:
+              switch (letter[3]) {
+                case 0:
+                  return 'C';
+                case 1:
+                  return 'Y';
+                default:
+                  return 'K';
+              }
+            default:
+              return 'N';
+          }
+        case 1:
+          switch (letter[2]) {
+            case 0:
+              switch (letter[3]) {
+                case 0:
+                  return 'Z';
+                case 1:
+                  return 'Q';
+                default:
+                  return 'G';
+              }
+            case 1:
+              return 'O';
+            default:
+              return 'M';
+          }
+        default:
+          return 'T';
+      }
+    default:
+      return '?';
+
   }
 }
 
@@ -62,27 +205,27 @@ void setup() {
 }
 
 void loop() {
-  char c;
+  String c;
 
   //reading only. will likely have to put in another file altogether
-  Serial.print(millis()/1000.0,0); //for testing (later)
+  Serial.print(millis() / 1000.0, 0); //for testing (later)
   Serial.print(',');
   Serial.print(analogRead(pr));
   Serial.print("\n");
   delay(10);
 
-  /*int newstate = readpr(dstate);
-  if(dstate == LOW && newstate == HIGH){
+  int newstate = readpr(dstate);
+  if (dstate == LOW && newstate == HIGH) {
     timer1start = millis();
     timer2stop = millis();
     timer2 = timer2stop - timer2start;
-    c = translate(timer2,0);
+    c = translate(timer2, 0);
   }
-  if(dstate == HIGH && newstate == LOW){
+  if (dstate == HIGH && newstate == LOW) {
     timer1stop = millis();
     timer2start = millis();
     timer1 = timer1stop - timer1start;
-    c = translate(timer1,1);
+    c = translate(timer1, 1);
   }
-  dstate = newstate;*/
+  dstate = newstate;
 }
--- MCRWrite/MCRWrite.ino
+++ MCRWrite/MCRWrite.ino
@@ -1,14 +1,23 @@
 /**
- * handles LED and writing morse code
- */
+   handles LED and writing morse code
+*/
 
- int unit = 500; // num milliseconds per unit
- int pr = 5; //pin for photoresistor
- int led = 1; //pin for LED
- static int dstate = 0; //assuming light starts off
+/*
+ *******************************************************
+    variables
+ *******************************************************
+*/
 
- int prlow = 400;
- int prhigh = 600;
+int unit = 500; // num milliseconds per unit
+int pr = 5; //pin for photoresistor
+int led = 1; //pin for LED
+
+
+/*
+*******************************************************
+   function declarations
+*******************************************************
+*/
 
 void dash();
 void dot();
@@ -43,204 +52,209 @@ void Z();
 void spaceW();
 void spaceL();
 
-void dash(){
+/*
+ *******************************************************
+    subroutines
+ *******************************************************
+*/
+
+void dash() {
   //three units of time for dash
-  digitalWrite(led,HIGH);
-  delay(unit*3);
+  digitalWrite(led, HIGH);
+  delay(unit * 3);
   //one unit of time for space between parts of the letter
-  digitalWrite(led,LOW);
+  digitalWrite(led, LOW);
   delay(unit);
 }
 
-void dot(){
+void dot() {
   //one unit of time
-  digitalWrite(led,HIGH);
+  digitalWrite(led, HIGH);
   delay(unit);
   //one unit of time for space between parts of the letter
-  digitalWrite(led,LOW);
+  digitalWrite(led, LOW);
   delay(unit);
 }
 
-void A(){
+void A() {
   dot();
   dash();
 }
 
-void B(){
+void B() {
   dash();
-  dot();
-  dot();
-  dot();
-}
-
-void C(){
-  dash();
-  dot();
-  dash();
-  dot();
-}
-
-void D(){
-  dash();
-  dot();
-  dot();
-}
-
-void E(){
-  dot();
-}
-
-void FF(){
-  dot();
-  dot();
-  dash();
-  dot();
-}
-
-void G(){
-  dash();
-  dash();
-  dot();
-}
-
-void H(){
-  dot();
   dot();
   dot();
   dot();
 }
 
-void I(){
-  dot();
-  dot();
-}
-
-void J(){
-  dot();
+void C() {
   dash();
-  dash();
-  dash();
-}
-
-void K(){
-  dash();
-  dot();
-  dash();
-}
-
-void L(){
-  dot();
-  dash();
-  dot();
-  dot();
-}
-
-void M(){
-  dash();
-  dash();
-}
-
-void N(){
-  dash();
-  dot();
-}
-
-void O(){
-  dash();
-  dash();
-  dash();
-}
-
-void P(){
-  dot();
-  dash();
-  dash();
-  dot();
-}
-
-void Q(){
-  dash();
-  dash();
-  dot();
-  dash();
-}
-
-void R(){
   dot();
   dash();
   dot();
 }
 
-void S(){
-  dot();
-  dot();
-  dot();
-}
-
-void T(){
-  dash();
-}
-
-void U(){
-  dot();
-  dot();
-  dash();
-}
-
-void V(){
-  dot();
-  dot();
-  dot();
-  dash();
-}
-
-void W(){
-  dot();
-  dash();
-  dash();
-}
-
-void X(){
-  dash();
-  dot();
-  dot();
-  dash();
-}
-
-void Y(){
-  dash();
-  dot();
-  dash();
-  dash();
-}
-
-void Z(){
-  dash();
+void D() {
   dash();
   dot();
   dot();
 }
 
-void spaceW(){
+void E() {
+  dot();
+}
+
+void FF() {
+  dot();
+  dot();
+  dash();
+  dot();
+}
+
+void G() {
+  dash();
+  dash();
+  dot();
+}
+
+void H() {
+  dot();
+  dot();
+  dot();
+  dot();
+}
+
+void I() {
+  dot();
+  dot();
+}
+
+void J() {
+  dot();
+  dash();
+  dash();
+  dash();
+}
+
+void K() {
+  dash();
+  dot();
+  dash();
+}
+
+void L() {
+  dot();
+  dash();
+  dot();
+  dot();
+}
+
+void M() {
+  dash();
+  dash();
+}
+
+void N() {
+  dash();
+  dot();
+}
+
+void O() {
+  dash();
+  dash();
+  dash();
+}
+
+void P() {
+  dot();
+  dash();
+  dash();
+  dot();
+}
+
+void Q() {
+  dash();
+  dash();
+  dot();
+  dash();
+}
+
+void R() {
+  dot();
+  dash();
+  dot();
+}
+
+void S() {
+  dot();
+  dot();
+  dot();
+}
+
+void T() {
+  dash();
+}
+
+void U() {
+  dot();
+  dot();
+  dash();
+}
+
+void V() {
+  dot();
+  dot();
+  dot();
+  dash();
+}
+
+void W() {
+  dot();
+  dash();
+  dash();
+}
+
+void X() {
+  dash();
+  dot();
+  dot();
+  dash();
+}
+
+void Y() {
+  dash();
+  dot();
+  dash();
+  dash();
+}
+
+void Z() {
+  dash();
+  dash();
+  dot();
+  dot();
+}
+
+void spaceW() {
   //space b/t words is supposed to be 7 in MC
-  //but that's too long so let's do 
-  digitalWrite(led,LOW);
-  delay(unit*5);
+  //but that's too long so let's do
+  digitalWrite(led, LOW);
+  delay(unit * 5);
 }
 
-void spaceL(){
-  digitalWrite(led,LOW);
-  delay(unit*2);
+void spaceL() {
+  digitalWrite(led, LOW);
+  delay(unit * 2);
 }
-
 
 
 void setup() {
-  pinMode(led,OUTPUT);
+  pinMode(led, OUTPUT);
 
 }
 
 void loop() {
-  
+
 
 }
